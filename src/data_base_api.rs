cb147777ef192a1fa942687b9e5b84d9a855c918
Willem Battey <115966859+willbtty@users.noreply.github.com>
2024-11-23 12:37:25 -0600

    Added a funciton for request login. Had to make a new one because old updates self.users

--- src/data_base_api.rs
+++ src/data_base_api.rs
@@ -1,5 +1,6 @@
 
 pub mod data_base_api{
+    use std::f32::consts::E;
     use std::sync::{Arc, Mutex};
     use serde::Deserialize;
     use reqwest::Client;
@@ -9,13 +10,15 @@ pub mod data_base_api{
     use core::error;
     use std::default;
     pub trait MakeRequest {
-        fn send_request(&self, input: &str) -> ReturnType;    
+        fn send_request(&self, input: &str) -> ReturnType;
+        fn login(&self, username: &str) -> Option<User>;
     }
 
     pub enum ReturnType{
         IsValid(bool),
         Users(Vec<User>),
         Error(Option<String>),
+        CurrentUser(User),
     }
 
     #[derive(Deserialize, Debug)]
@@ -42,21 +45,28 @@ pub mod data_base_api{
                 client: Client::new(),
                 notify: Arc::new(Notify::new()),
                 friends: Arc::new(Mutex::new(Vec::new())),
-                users: {Arc::new(Mutex::new(Vec::new()))}
+                users: {Arc::new(Mutex::new(Vec::new()))},
             };
             api.send_request("/api/User/GetAllUsers");
 
             api
         }
         
-        pub fn login_request(&self, username: &str) -> bool {
-            let ending_str = format!("/api/User/GetUserLogin={}", username);
-            let user_data = self.send_request(username);
-
-        }
-
+        pub fn login_request(&self, username: &str, password: &str) -> bool {
+            let user = self.login(username);
+            match user {
+                Some(u) => {
+                    if u.Password != password {
+                        false
+                    }
+                    else {
+                        true
+                    }
+                },
+                None => false,
+            }
     }
-
+}
 
     impl MakeRequest for DbAPI{
         fn send_request(&self, input: &str) -> ReturnType{
@@ -73,6 +83,23 @@ pub mod data_base_api{
                     Err(e) => eprint!("{}", e)}          
             });
             ReturnType::Error(None)
+        }
+
+        fn login (&self, username: &str) -> Option<User> {
+            let url = format!("http://word-unscrambler-api-ade3e9ard4huhmbh.canadacentral-01.azurewebsites.net/api/User/LookForUser?username={}", username);
+            let user: Result<User, reqwest::Error>;
+            tokio::spawn(async move {
+                let response = reqwest::get(&url).await;
+                match response {
+                    Ok(resp) => {
+                        let user: User = resp.json().await.unwrap();
+                        Ok(user)
+                    },
+                    Err(e) => Err(e),
+                }
+            });
+            None
+
         }
     }
 }

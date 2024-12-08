01b0a281742a008dc923714f96856193aea005f6
yung00se <johnmkmosley@gmail.com>
2024-12-08 02:15:50 -0600

    Skeleton of Chat feature

--- src/chat_bar.rs
+++ src/chat_bar.rs
@@ -1,16 +1,44 @@
 use eframe::egui;
 use crate::vapor::Vapor;
+use std::{fs::{self, File}, io::{BufRead, BufReader, Write}, process::{Child, ChildStdin, ChildStdout, Command, Stdio}, sync::{Arc, Mutex}, time::Duration};
 
 pub struct Chat{
-    buffer: Vec<String>,
+    buffer: Arc<Mutex<Vec<String>>>,
     message: String,
+    child_stdin: ChildStdin,
 }
 
 impl Chat{
     pub fn new() -> Self{
+        let mut child = Command::new("python")
+            .arg("./python_client/main.py")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failed to start python script");
+
+        let buffer: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+        let buffer_clone = Arc::clone(&buffer);
+        //let child_stdout = child.stdout.expect("Failed to get stdout of child");
+        
+
+        std::thread::spawn(move || {
+            if let Some(chat_output) = &mut child.stdout {
+                let reader = BufReader::new(chat_output).lines().enumerate().take(128);
+
+                for (size, line) in reader{
+                    //eprint!("im here");
+                    let output = line.expect("Failed");
+                    eprint!("[{}]", output);
+                }
+            }
+            else {}
+        });
+
         Self{
-            buffer: Vec::new(),
+            buffer,
             message: String::new(),
+            child_stdin: child.stdin.take().expect("Failed to get stdin of child"),
         }
     }
 }
@@ -20,9 +48,10 @@ pub trait ChatBar{
 }
 
 impl ChatBar for Chat {
-    fn display_chat_bar(&mut self, ctx: &egui::Context){
+    fn display_chat_bar(&mut self, ctx: &egui::Context) {
         egui::Window::new("Chat").show(ctx, |ui| {
-            for message in &self.buffer{
+            let buffer = self.buffer.lock().unwrap();
+            for message in buffer.iter() {
                 ui.add(egui::Label::new(message));
             }
 
@@ -30,7 +59,7 @@ impl ChatBar for Chat {
                 egui::TextEdit::singleline(&mut self.message)
                     .hint_text("Type here..."));
             if ui.input(|i|{ i.key_pressed(egui::Key::Enter) }) {
-                self.buffer.push(self.message.clone());
+                //self.buffer.push(self.message.clone());
                 self.message.clear();
             }
         });

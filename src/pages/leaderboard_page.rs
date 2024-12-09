001ccd4786144249b4a15f5a41b244d290c7b775
Willem Battey <115966859+willbtty@users.noreply.github.com>
2024-12-08 19:48:53 -0600

    Got Rapid Math leader board working

--- src/pages/leaderboard_page.rs
+++ src/pages/leaderboard_page.rs
@@ -1,4 +1,5 @@
 use eframe::egui::{self, mutex::MutexGuard, Align, CentralPanel, Color32, FontId, Grid, Label, Layout, RichText, ScrollArea, Sense, TextStyle, TopBottomPanel, Vec2};
+use ::egui::plot::Text;
 //use crate::user_info::User;
 use crate::{data_base_api::{DbAPI, UserEntry}, vapor::Vapor};
 use std::sync::Mutex;
@@ -34,6 +35,13 @@ impl Leaderboard {
                 if ui
                     .add(Label::new(RichText::new("Sudoku").text_style(TextStyle::Heading)).sense(Sense::click()))
                     .clicked() { self.current_page = "Sudoku".into()  } /*End Login/Signup Buttons*/ });
+
+                ui.add_space(75.0);
+
+                if ui
+                    .add(Label::new(RichText::new("Rapid Math").text_style(TextStyle::Heading)).sense(Sense::click()))
+                    .clicked() { self.current_page = "Rapid Math".into() }
+                println!("HELLO");
         });
         let ws_leaderboard = &self.db_api.leaderboard.lock().unwrap();
         let s_leaderboard = &self.db_api.sudoku_leaderboard.lock().unwrap();
@@ -123,10 +131,19 @@ impl DisplayLeaderboard for Vapor {
 
                 if ui
                     .add(Label::new(RichText::new("Sudoku").text_style(TextStyle::Heading)).sense(Sense::click()))
-                    .clicked() { self.leaderboard.current_page = "Sudoku".into()  } /*End Login/Signup Buttons*/ });
+                    .clicked() { self.leaderboard.current_page = "Sudoku".into()  } /*End Login/Signup Buttons*/
+                ui.add_space(75.0);
+                
+                if ui
+                    .add(Label::new(RichText::new("Rapid Math").text_style(TextStyle::Heading)).sense(Sense::click()))
+                    .clicked() { 
+                        self.leaderboard.current_page = "Rapid Math".into();
+                }
+            });
         });
         let ws_leaderboard = &self.db_api.leaderboard.lock().unwrap();
         let s_leaderboard = &self.db_api.sudoku_leaderboard.lock().unwrap();
+        let m_leaderboard = &self.db_api.math_leaderboard.lock().unwrap();
 
         egui::CentralPanel::default().show(ctx, |ui| {
             let mut index = 0;
@@ -190,6 +207,39 @@ impl DisplayLeaderboard for Vapor {
                                 break
                             }
                             ui.label(user.HighScoreSudoku.to_string());
+                            ui.add_space(10.0);
+                            index += 1;
+                        }
+                    });
+                });
+            }
+            else if self.leaderboard.current_page == "Rapid Math".to_string(){
+                ui.horizontal(|ui| {
+                    ui.add_space(200.0);
+                    ui.vertical(|ui| {
+                        ui.heading("User");
+                        ui.add_space(10.0);
+                        
+                        for user in m_leaderboard.iter() {
+                            if index > 9 {
+                                break
+                            }
+                            ui.label(user.Username.clone());
+                            ui.add_space(10.0);
+                            index += 1;
+                        }
+                    });
+
+                    ui.add_space(100.0);
+                    ui.vertical(|ui| {
+                        ui.heading("High Score");
+                        ui.add_space(10.0);
+                        index = 0;
+                        for user in m_leaderboard.iter() {
+                            if index > 9 {
+                                break
+                            }
+                            ui.label(user.HighScoreMath.to_string());
                             ui.add_space(10.0);
                             index += 1;
                         }
